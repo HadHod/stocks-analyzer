7c09def037c917d7d89d3e9f9b820f430eeaa905
HadHod <maciej.mlynarski@gmail.com>
2021-03-30 17:34:50 +0200

    tooltip override

--- src/app/analysis/companies/usa/gold/gold.component.ts
+++ src/app/analysis/companies/usa/gold/gold.component.ts
@@ -1,4 +1,5 @@
-import { Component, ChangeDetectionStrategy, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
+import { ChartTooltipModel } from 'chart.js';
 
 @Component({
   templateUrl: './gold.component.html',
@@ -6,20 +7,25 @@ import { Component, ChangeDetectionStrategy, AfterViewInit, ViewChild, ElementRe
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GoldComponent {
-  @ViewChild('dividendsCanvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
-
   public barChartOptions = {
-    scaleShowVerticalLines: false,
+    scaleShowVerticalLines: true,
     responsive: true,
+    tooltips: {
+      custom: (tooltipModel: ChartTooltipModel) => {
+        tooltipModel.title = ['DGR2Y: 10%', 'DGR5Y: 10%'];
+        tooltipModel.height = 60;
+        tooltipModel.width = 82;
+      },
+    }
   };
 
   public barChartLabels = [
     '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020',
   ];
   public barChartType = 'bar';
-  public barChartLegend = true;
+  public barChartLegend = false;
   public barChartData = [
-    {data: [0.22, 0.22, 0.3, 0.04, 0.04, 0.26, 0.51, 0.21, 0.14, 0.2, 0.14, 0.08, 0.12, 0.19, 0.13, 0.31], label: 'Dividends'},
+    { data: [0.22, 0.22, 0.3, 0.04, 0.04, 0.26, 0.51, 0.21, 0.14, 0.2, 0.14, 0.08, 0.12, 0.19, 0.13, 0.31] },
   ];
 
   public barChartOptions2 = {
@@ -33,7 +39,7 @@ export class GoldComponent {
   public barChartType2 = 'bar';
   public barChartLegend2 = true;
   public barChartData2 = [
-    {data: [2348, 5630, 6014, 7613, 8136, 11001, 14236, 14394, 12527, 10239, 9029, 8558, 8374, 7243, 9717, 12595], label: 'Revenue'},
-    {data: [1150, 2920, 3016, 3907, 4329, 5839, 7996, 7137, 5198, 3409, 2122, 3153, 3074, 2023, 2806, 5178], label: 'Gross profit'},
+    { data: [2348, 5630, 6014, 7613, 8136, 11001, 14236, 14394, 12527, 10239, 9029, 8558, 8374, 7243, 9717, 12595], label: 'Revenue' },
+    { data: [1150, 2920, 3016, 3907, 4329, 5839, 7996, 7137, 5198, 3409, 2122, 3153, 3074, 2023, 2806, 5178], label: 'Gross profit' },
   ];
 }
